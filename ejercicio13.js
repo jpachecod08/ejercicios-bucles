d70634e6a9060f2b8a3d7ec269b61de9fac7a4d9
jesus <daviddominiqueti21@gmail.com>
2024-03-12 10:04:37 -0500

    Mensaje de commit

--- ejercicio13.js
+++ ejercicio13.js
@@ -1,14 +1,18 @@
 process.stdin.setEncoding('utf8');
-
 process.stdout.write('Introduce una frase : ');
 process.stdin.on('data', function(data) {
-    const frase = data.toString().trim();
+const frase = data.toString().trim();    
+while(frase !== ''){
     if (frase.toLowerCase() === 'salir') {
+        console.log('Frase ingresada:', frase);
         process.exit(); 
     } else {
         console.log('Frase ingresada:', frase);
-        process.stdout.write('Introduce otra frase : ');
+        process.stdout.write('Introduce una frase : ');    
     }
+    break
+  }
+
 });
 
 
